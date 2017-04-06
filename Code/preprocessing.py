a14978f74c649695c09e1184e3a16b073b73ed7a
selBaez <sel.bs.93@hotmail.com>
2017-04-06 11:04:56 +0200

    parsing routes

--- Code/preprocessing.py
+++ Code/preprocessing.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -9,14 +11,28 @@ import matplotlib.pyplot as plt
 
 ############ --- BEGIN default constants --- ############
 FILE_NAME_DEFAULT = '../Data/Travel chain sample data(50000).csv'
+TRANSLATE_DICT_DEFAULT = {  '轨道' : 'R',
+                            '公交' : 'B',
+                            '自行车' : 'Z',
+                            '线' : 'Line',
+                            '号' : '',
+                            '夜' : 'Night ',
+                            '站' : 'Station ',
+                            '小区' : 'District ',
+                            '机场' : 'Airport ',
+                            '公交场' : 'Bus loop ',
+                            '北' : 'North ',
+                            '南' : 'South ',
+                            '东' : 'East ',
+                            '西' : 'West '}
 ############ --- END default constants--- ############
 
 def _loadData(fileName):
     """
-    Load csv data on numpy
+    Load csv data on pandas
     """
     # Ignore column 2 'DATA_LINK'
-    fullData = pd.read_csv(fileName, index_col='ID', usecols= range(2)+range(3,23), parse_dates=[8,9])
+    fullData = pd.read_csv(fileName, index_col='ID', usecols= range(2)+range(3,23), parse_dates=[0,8,9])
     print(len(fullData.index), " records loaded")
 
     return fullData
@@ -28,38 +44,42 @@ def _clean(fullData):
     # Remove rows containing NaN
     noMissing = fullData.dropna()
     missingRecords_num = len(fullData.index) - len(noMissing.index)
-
     print(missingRecords_num, " records removed due to empty fields, ", len(noMissing.index), " records left")
 
     # Remove records with travel time <= 0
     timeRelevant = noMissing[noMissing['TRAVEL_TIME'] > 0]
     timeFaultyRecords_num = len(noMissing.index) - len(timeRelevant.index)
-
     print(timeFaultyRecords_num, " records removed due to travel time <= 0, ", len(timeRelevant.index), " records left")
 
     # Remove records with travel distance <= 0
     #some still make sense in terms of time and transfers
     distanceRelevant = timeRelevant[timeRelevant['TRAVEL_DISTANCE'] > 0]
     distanceFaultyRecords_num = len(timeRelevant.index) - len(distanceRelevant.index)
-
     print(distanceFaultyRecords_num, " records removed due to travel distance <= 0, ", len(distanceRelevant.index), " records left")
 
+    # Remove rows with strange transfers
+    # TODO
 
-    #return cleanData
+    return distanceRelevant
 
-def _parseRoute(data):
+def _parseRoute(data, chineseDict):
     """
     Parse 'TRANSFER_DETAIL' column to get line
     """
     #TODO find way to split by - (representing a transfer)
-    data['TRANSFER_DETAIL'] = data['TRANSFER_DETAIL'].str.slice(0,10)
-    pass
+    print("Replacing basic Chinese according to dictionary")
+    for key in reversed(sorted(chineseDict.keys())):
+        data['TRANSFER_DETAIL'] = data['TRANSFER_DETAIL'].str.replace(key,chineseDict[key])
+
+    return data
 
 def _whitening(rawData):
     """
-    remove correlations and fit to variance 1
+    Remove correlations and fit to variance 1
     """
-    pass
+    print("Standarize travel time")
+    print("Standarize travel distance")
+
 
 def _store(preprocessedData):
     """
@@ -78,10 +98,13 @@ def preprocess():
     cleanData = _clean(fullData)
 
     print("-------------------------- Parse  route ---------------------------")
+    withRouteData = _parseRoute(cleanData, TRANSLATE_DICT_DEFAULT)
 
     print("------------------------ Extract weekdays -------------------------")
+    # TODO http://nbviewer.jupyter.org/github/jvns/pandas-cookbook/blob/v0.1/cookbook/Chapter%204%20-%20Find%20out%20on%20which%20weekday%20people%20bike%20the%20most%20with%20groupby%20and%20aggregate.ipynb
 
     print("---------------------------- Whitening ----------------------------")
+    preprocessedData = _whitening(withRouteData)
 
     print("--------------------------- Store  data ---------------------------")
 
