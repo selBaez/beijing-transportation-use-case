3ce9d5f7fa0bfc1a9033c5224e33e088f6f3603d
selBaez <sel.bs.93@hotmail.com>
2017-04-07 19:17:46 +0200

    eliminate users with insufficient trips

--- Code/preprocessing.py
+++ Code/preprocessing.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 
 ############ --- BEGIN default constants --- ############
 FILE_NAME_DEFAULT = '../Data/Travel chain sample data(50000).csv'
+MIN_RECORDS_DEFAULT = 2
+PLOT_DISTR_DEFAULT = True
 TRANSLATE_DICT_DEFAULT = {  '轨道' : 'R',
                             '公交' : 'B',
                             '自行车' : 'Z',
@@ -37,7 +39,7 @@ def _loadData(fileName):
 
     return fullData
 
-def _clean(fullData):
+def _clean(fullData, min_records):
     """
     Remove rows with faulty data
     """
@@ -57,26 +59,45 @@ def _clean(fullData):
     distanceFaultyRecords_num = len(timeRelevant.index) - len(distanceRelevant.index)
     print(distanceFaultyRecords_num, " records removed due to travel distance <= 0, ", len(distanceRelevant.index), " records left")
 
-    # Remove rows with strange transfers
-    # TODO
+    # TODO Remove rows with strange transfers
+    #print(distanceFaultyRecords_num, " records removed due to num of transfer >= 5, ", len(distanceRelevant.index), " records left")
 
-    return distanceRelevant
+    # Remove cards with less than min_records
+    distanceRelevant['NUM_TRIPS'] = distanceRelevant.groupby('CARD_CODE')['TRAVEL_DISTANCE'].transform('count')
+    cleanData = distanceRelevant[distanceRelevant['NUM_TRIPS'] >= min_records]
+    notEnoughRecords_num = len(distanceRelevant.index) - len(cleanData.index)
+    print(notEnoughRecords_num, " records removed due to insufficient users having associated records, ", len(cleanData.index), " records left")
+
+    return cleanData
 
 def _parseRoute(data, chineseDict):
     """
     Parse 'TRANSFER_DETAIL' column to get line
     """
     #TODO find way to split by - (representing a transfer)
-    print("Replacing basic Chinese according to dictionary")
+
+    # Translate basic keywords
+    print("Replacing keywords from Chinese to English")
     for key in reversed(sorted(chineseDict.keys())):
         data['TRANSFER_DETAIL'] = data['TRANSFER_DETAIL'].str.replace(key,chineseDict[key])
 
+    # TODO: Extract stop number (if available) and line route as S3->B56
+    #print("Simplifying route")
+
     return data
 
-def _whitening(rawData):
+def _whitening(rawData, plot_distr):
     """
     Remove correlations and fit to variance 1
     """
+
+    if plot_distr:
+        print("Original travel time distribution")
+        temp = rawData.sort_values(by='TRAVEL_TIME')
+        fig, ax = plt.subplots()
+        temp[:1000].plot(y='TRAVEL_TIME', ax=ax)
+        plt.show()
+
     print("Standarize travel time")
     print("Standarize travel distance")
 
@@ -95,16 +116,21 @@ def preprocess():
     fullData = _loadData(FLAGS.file_name)
 
     print("---------------------------- Cleaning -----------------------------")
-    cleanData = _clean(fullData)
+    cleanData = _clean(fullData, FLAGS.min_records)
+
+    ########################### Feature  engineering ###########################
 
     print("-------------------------- Parse  route ---------------------------")
     withRouteData = _parseRoute(cleanData, TRANSLATE_DICT_DEFAULT)
+
+    print("----------------------- Creating time bins ------------------------")
+    #TODO
 
     print("------------------------ Extract weekdays -------------------------")
     # TODO http://nbviewer.jupyter.org/github/jvns/pandas-cookbook/blob/v0.1/cookbook/Chapter%204%20-%20Find%20out%20on%20which%20weekday%20people%20bike%20the%20most%20with%20groupby%20and%20aggregate.ipynb
 
     print("---------------------------- Whitening ----------------------------")
-    preprocessedData = _whitening(withRouteData)
+    preprocessedData = _whitening(withRouteData, FLAGS.plot_distr)
 
     print("--------------------------- Store  data ---------------------------")
 
@@ -128,6 +154,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_name', type = str, default = FILE_NAME_DEFAULT,
                         help='Data file to load.')
+    parser.add_argument('--min_records', type = int, default = MIN_RECORDS_DEFAULT,
+                        help='Traveler is required to have at least this number of records.')
+    parser.add_argument('--plot_distr', type = bool, default = PLOT_DISTR_DEFAULT,
+                        help='Plot time and distance distributions.')
 
     FLAGS, unparsed = parser.parse_known_args()
     main(None)
