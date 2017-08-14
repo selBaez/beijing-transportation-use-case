81254950cab5b722529b1356a0780757d0f24bbc
selBaez <sel.bs.93@hotmail.com>
2017-08-14 17:27:42 +0200

    tsne tu's features

--- Code/plotFromTxt.py
+++ Code/plotFromTxt.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import os
+from sklearn.manifold import TSNE
 
 
 
@@ -17,7 +18,7 @@ import os
 # shared._sampleCalculateStd(times[:,0], times[:,1], 'Number of cores', 'Time (s)', 'Parallel implementation')
 
 # Plot volume of data
-volume = np.loadtxt('Logs/preparation/dataVolume.txt')
+# volume = np.loadtxt('Logs/preparation/dataVolume.txt')
 # shared._volumeBar(volume, 'Daily volume of records')
 # total = np.sum(volume[:,2])
 # print(total)
@@ -26,3 +27,18 @@ volume = np.loadtxt('Logs/preparation/dataVolume.txt')
 # vocabularyFile = np.loadtxt('Logs/preparation/cummulativeVocabulary.txt')
 # shared._vocabCum(vocabularyFile[:,:-1], 'Cummulative lines vocabulary size')
 # shared._vocabCum(vocabularyFile[:,[0,2]], 'Cummulative stops vocabulary size')
+
+# Plot tsne of samples from Tu
+# # Read data
+# dataCom = np.loadtxt(paths.PREVIOUS_DIR_DEFAULT+'Commuter_processed_data.txt')[:, :-1]
+# dataNonCom = np.loadtxt(paths.PREVIOUS_DIR_DEFAULT+'NonCommuter_processed_data.txt')[:, :-1]
+# # Create labels
+# labelsCom = np.ones(dataCom.shape[0])
+# labelsNonCom = np.zeros(dataNonCom.shape[0])
+# # Concatenate
+# data = np.concatenate((dataCom, dataNonCom))
+# labels = np.concatenate((labelsCom, labelsNonCom))
+# # Plot
+# manifold = TSNE(n_components=2, random_state=0)
+# features = manifold.fit_transform(data)
+# shared._tsneScatter("Tu's features", features, labels=labels)
